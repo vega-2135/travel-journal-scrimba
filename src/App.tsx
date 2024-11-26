37e06990af4ea798ad47e455795afd3450bf5b1e
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-11-26 15:15:58 +0100

    Add Entry component
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
 import Header from './components/Header'
+import Entry from './components/Entry'
 import './App.css'
 
 function App() {
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Entry />
     </>
   )
 }
