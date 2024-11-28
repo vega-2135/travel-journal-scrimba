f09acbfca4c8500c8d844a52f11208ebcf98708b
Carolina Romero Carrasco <mcarolinarc28@gmail.com>
2024-11-28 14:26:10 +0100

    Add props to Entry component
    Signed-off-by: Carolina Romero Carrasco <mcarolinarc28@gmail.com>

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,24 +1,22 @@
 
 import location from '../assets/images/location.webp'
-import japan from '../assets/images/japan.jpg'
 
-export default function Entry() {
+export default function Entry(props) {
     return (
         <>
             <article className='main-container'>
-                <div className='location-img-container'>
-                    <img className='site-img' src={japan} alt="mount-fuji" />
+                <div>
+                    <img className='location-img-container' src={props.image.src} alt={props.image.alt} />
                 </div>
                 <div className='content'>
                     <img src={location} className='location' alt="location icon" />
-                    <span className='japan'>JAPAN</span>
-                    <a className='link' href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
+                    <span className='japan'>{props.country}</span>
+                    <a className='link' href={props.googleMapsLink}>
                         View on Google Maps
                     </a>
-                    <h2>Mount Fuji</h2>
-                    <p className='date'>12 Jan, 2021 - 24 Jan, 2021</p>
-                    <p className='description'>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). 
-                    Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                    <h2>{props.title}</h2>
+                    <p className='date'>{props.date}</p>
+                    <p className='description'>{props.text}</p>
                 </div>
             </article>
         </>
